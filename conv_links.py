2460c26b063a991313de219b9ff8f1762c976c54
berak <px1704@web.de>
2018-08-15 10:05:11 +0200

    profile, link generator fixed

--- conv_links.py
+++ conv_links.py
@@ -8,6 +8,7 @@ for t in tab:
 	print (t['href'])
 	fi = open(t['href'],"rb")
 	txt = fi.read()
+	fi.close()
 	start=i-3
 	if i<4: start=0
 	if i>len(tab)-4: start = len(tab)-7
@@ -16,16 +17,17 @@ for t in tab:
 	while(j<7):
 	   lnks += str(tab[j+start]) + "<br>\r\n"
 	   j += 1
-	print(lnks)
+	#print(lnks)
 	dc = "<div class=\"nav\">"
 	start = txt.find(dc)
 	end = txt[start:].find("</div>")
-	htm = txt[start+len(dc):] + "\r\n"
+	#print(start,end, len(lnks))
+	htm = txt[:start+len(dc)] + "\r\n"
 	htm += lnks
 	htm += txt[start+end:]
-	fi.close()
+	#print(htm)
 	fo = open(t['href'],"wb")
-	fo.write(txt)
+	fo.write(htm)
 	fo.close()
 	i += 1
 #help(soup)
